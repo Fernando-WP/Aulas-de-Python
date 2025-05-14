c3eb397a86b15b96de1457d3f19bd9a671bd116f
Fernando Brasil Souza <brasilsouzafernando493@gmail.com>
2025-05-14 18:03:33 -0300

    Caixa Eletrônico
    Esse código uma caixa eletrônico, o usuário pode depositar, sacar, ver o saldo , sacar em crédito e ver o saldo do crédito. O código repete sozinha salvando os dados do usuário, pra para o código usuário deve digita 5 = 'sair' no programar.

--- Caixa Eletrônico.py
+++ Caixa Eletrônico.py
@@ -1,50 +1,76 @@
+import os
+
 Saldo = 0
 
-Crédito = 0 
+Crédito = 0
 
-while True:
+def exibir_opções():
 
     print('''=======Caixa Eletrônico========
-[ 1 ] - Ver Saldo
-[ 2 ] - Depositar
-[ 3 ] - Sacar Saldo
-[ 4 ] - Ver crédito
-[ 5 ] - Sair''')
+    [ 1 ] - Ver Saldo
+    [ 2 ] - Depositar
+    [ 3 ] - Sacar Saldo
+    [ 4 ] - Ver crédito
+    [ 5 ] - Sair''')
     print('=' * 31)
 
-    Opção = int(input('Escolha uma das opções de cima: '))
+def opcao_invalida():
+    os.system('cls')
+    print('Opção Inválida. TENTA NOVAMENTE!\n')
+    input('|Pressionar qualquer teclar para voltar| ')
+    os.system('cls')
+    main()
 
-    print('~=' * 30)
-    if Opção == 1:
-        print(f'Seu saldo atual: R${Saldo:.2f}')
+def saindo_do_caixa_eletrônica():
+    os.system('cls')
+    print('Saindo . . . Tenha um ótimo dia.\n')
 
-    elif Opção == 2:
-        Valor_de_Deposito = float(input('Quanto você quer depositar na sua conta? R$'))
-        Saldo += Valor_de_Deposito
-        print(f'Você depositou R${Valor_de_Deposito:.2f}.')
+def escolhar_de_opções():
+    global Saldo, Crédito
 
-    elif Opção == 3:
-        Valor_de_Saque = float(input('Quanto você quer sacar da sua conta? R$'))
-        Saldo -= Valor_de_Saque
-        print(f'Você sacou R${Valor_de_Saque:.2f}.')
+    try: 
+        Opcao = int(input('\nEscolha uma das opções de cima: '))
 
-        if Saldo < 0:
-            Crédito_de_Saque = str(input('Saldo Insuficiente. Você quer sacar em crédito? (sim/não): '))
+        if Opcao == 1:
+            print(f'\nSeu saldo atual: R${Saldo:.2f}\n')
 
-            if Crédito_de_Saque in 'sim':
-                Valor_de_Crédito = Valor_de_Saque - Saldo
-                Crédito += Valor_de_Crédito
-                print(f'Você está usando R${Valor_de_Crédito:.2f} de crédito.')
-                Saldo = 0
-                print(f'Seu saldo: R${Saldo:.2f}')
+        elif Opcao == 2:
+            Valor_de_Deposito = float(input('\nQuanto você quer depositar na sua conta? R$'))
+            Saldo += Valor_de_Deposito
+            print(f'\nVocê depositou R${Valor_de_Deposito:.2f}\n')
 
-    elif Opção == 4:
-        print(f'Seu saldo de crédito atual: R${Crédito:.2f}')
+        elif Opcao == 3:
+            Valor_de_Saque = float(input('\nQuanto você quer sacar da sua conta? R$'))
+            Saldo -= Valor_de_Saque
+            print(f'\nVocê sacou R${Valor_de_Saque:.2f}.\n')
 
-    elif Opção == 5:
-        print('Saindo... Tenha um ótimo dia.')
-        break
+            if Saldo < 0:
+                Crédito_de_Saque = str(input('Saldo Insuficiente. Você quer sacar em crédito? (sim/não): '))
 
-    else:
-        print('Opção Inválida. TENTA NOVAMENTE!')
-    print('~=' * 30)
+                if Crédito_de_Saque in 'sim':
+                    Valor_de_Crédito = Valor_de_Saque - Saldo
+                    Crédito += Valor_de_Crédito
+                    print(f'\nVocê está usando R${Valor_de_Crédito:.2f} de crédito.')
+                    Saldo = 0
+                    print(f'\nSeu saldo: R${Saldo:.2f}\n')
+
+        elif Opcao == 4:
+            print(f'\nSeu saldo de crédito atual: R${Crédito:.2f}\n')
+
+        elif Opcao == 5:
+            saindo_do_caixa_eletrônica()
+            return
+                    
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+
+    main()
+    
+def main():
+    exibir_opções()
+    escolhar_de_opções()
+
+if __name__ == '__main__':
+    main()
