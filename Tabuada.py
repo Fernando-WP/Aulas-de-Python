66df19bdff17499c62102e0fefd6a68a56014269
Fernando Brasil Souza <brasilsouzafernando493@gmail.com>
2025-05-15 22:52:02 -0300

    Tabuada
    O usuário pode digita quantas vazes quer multiplica, e qual número que ele quer que multiplica. O código formar uma tabuada de vezes completo.
    
    Atualização: Agora o usuário pode escolhe o operadores, podendo multiplica, soma, subtrai ou dividi.

--- Tabuada.py
+++ Tabuada.py
@@ -1,5 +1,11 @@
 print('~-' * 20)
-n1 = int(input('Digita quantas vazes você quer multiplica: '))
+print('''=========OPERADORES========
+[ x ] - Multiplicação
+[ + ] - Soma
+[ - ] - Subtração
+[ / ] - Divisão''')
+operadores = str(input('\nEscolha um dos operadores: '))
+n1 = int(input('\nDigita quantas vazes você quer multiplica: '))
 n2 = int(input('\nDigita o número que você quer multiplica: '))
 print('~-' * 20)
 
@@ -8,6 +14,16 @@ print(f'{tx:^20}')
 
 print('=-' * 10)
 for c in range(1, n1+1):
-    t = n2 * c
-    print(f'{n2} X {c} = {t}')
+    if operadores == 'x':
+        M = n2 * c
+        print(f'{n2} X {c} = {M}')
+    elif operadores == '+':
+        M = n2 + c
+        print(f'{n2} + {c} = {M}')
+    elif operadores == '-':
+        M = n2 - c
+        print(f'{n2} - {c} = {M}')
+    elif operadores == '/':
+        M = n2 / c
+        print(f'{n2} / {c} = {M}')
 print('=-' * 10)
